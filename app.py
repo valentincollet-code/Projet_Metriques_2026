02f8f51e9689cbbc7869ae8c1bbf8eac80cf9ed3
valentincollet-code <valentin.collet@2028.icam.fr>
2026-04-10 15:05:15 +0200

    Remove hello_world route from app.py
    Removed the default route and hello_world function.

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import requests
 from flask import Flask, jsonify, render_template
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return render_template('hello.html')
 
 # Déposez votre code à partir d'ici :
 
