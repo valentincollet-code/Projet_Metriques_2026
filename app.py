604eb9024c5fe94786eb618a0285c107b8ca5992
valentincollet-code <v.colletpro@gmail.com>
2026-04-10 14:29:37 +0200

    Add contact route with simple response

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ def hello_world():
     return render_template('hello.html')
 
 # Déposez votre code à partir d'ici :
-
+@app.route("/contact")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"  
 
 
 
