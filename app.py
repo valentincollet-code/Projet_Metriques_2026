fa5f4b04f584c6e756ca9b60efdd3e5ad03e21d2
valentincollet-code <valentin.collet@2028.icam.fr>
2026-04-10 14:34:38 +0200

    Add API endpoint for Paris temperature forecast
    Added a new API endpoint to fetch hourly temperature data for Paris.

--- app.py
+++ app.py
@@ -7,6 +7,24 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
+@app.get("/paris")
+def api_paris():
+    
+    url = "https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&hourly=temperature_2m"
+    response = requests.get(url)
+    data = response.json()
+
+    times = data.get("hourly", {}).get("time", [])
+    temps = data.get("hourly", {}).get("temperature_2m", [])
+
+    n = min(len(times), len(temps))
+    result = [
+        {"datetime": times[i], "temperature_c": temps[i]}
+        for i in range(n)
+    ]
+
+    return jsonify(result)
+
 # Déposez votre code à partir d'ici :
 @app.route("/contact")
 def MaPremiereAPI():
