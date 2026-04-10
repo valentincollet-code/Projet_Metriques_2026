3ce025144fedf893b4595f4558748498a071e9c3
valentincollet-code <valentin.collet@2028.icam.fr>
2026-04-10 15:04:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+import requests
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
+
+# Déposez votre code à partir d'ici :
+
 @app.get("/paris")
 def api_paris():
     
@@ -15,3 +26,10 @@ def api_paris():
     ]
 
     return jsonify(result)
+
+
+
+# Ne rien mettre après ce commentaire
+    
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000, debug=True)
